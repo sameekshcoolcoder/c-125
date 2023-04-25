e18282e198fc525bd70721857adc9f21244e9012
sameekshcoolcoder <113428648+sameekshcoolcoder@users.noreply.github.com>
2023-04-25 19:20:43 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX= 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#969A97')
+
+    document.getElementById("text_size").innerHTML = "Width and Height of a text will be = " + difference + "px";
+    fill('#F90093');
+    textSize(difference);
+    text('Peter', 50, 308);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
